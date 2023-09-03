203bb8098988b976645624aef206e5070f62cc3a
Samuel Tovo <samuel.gatti.tovo@gmail.com>
2023-09-03 16:00:58 -0300

    fixing dag issues

--- dags/rent_to_s3.py
+++ dags/rent_to_s3.py
@@ -1,18 +1,14 @@
 
 import os
 import logging
-from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python_operator import PythonOperator
 from custom_modules.rent_extractor.apartment_extractor import GetApartmentsInfo
 import io
-
-
-import awswrangler as wr
+import s3fs
 import pandas as pd
-from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
-
+from datetime import date 
 
 
 
@@ -28,23 +24,15 @@ def define_page_range_for_request():
     return first_page, last_page
 
 
-def extract_rent(**kwargs):
-    # ti = kwargs['ti']
+def extract_rent_etl():
     first_page, last_page = define_page_range_for_request()
     extraction=GetApartmentsInfo(first_page,last_page)
     extraction_pd=extraction.generate_pandas_apartment_info()
-    print(extraction_pd)
-    return extraction_pd
-    # ti.xcom_push(key = 'rent_extraction' , value = extraction_pd.to_parquet(s3_url))
+    extraction_pd.to_parquet(f"s3://rent-extraction/bronze/rent_extraction_{date.today()}.parquet")
+
+    
 
 
-def upload_to_s3():
-    df: pd.DataFrame =  extract_rent()
-    key = 'bronze/rent_extraction.parquet'
-    hook = AwsBaseHook(aws_conn_id="AWS_CONN")
-
-    # upload to S3 bucket
-    wr.s3.to_parquet(df=df, path=f"s3://rent-extraction/{key}", boto3_session=hook.get_session())
 
 with DAG(
     dag_id="rent_to_s3",
@@ -55,13 +43,8 @@ with DAG(
    
     extract_rent_to_parquet = PythonOperator(
         task_id = 'extract_rent_to_parquet',
-        python_callable = extract_rent,
+        python_callable = extract_rent_etl,
         provide_context = True, 
     )
-    upload_prices_to_s3 = PythonOperator(
-        task_id = 'upload_prices_to_s3',
-        python_callable = upload_to_s3,
-        provide_context = True, 
-    )
-    extract_rent_to_parquet >> upload_prices_to_s3
+    extract_rent_to_parquet
 

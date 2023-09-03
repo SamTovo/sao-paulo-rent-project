3d627f58a8ffee889eb823239fa503285ced5dfc
Samuel Tovo <samuel.gatti.tovo@gmail.com>
2023-09-03 17:04:54 -0300

    adding logging debug

--- dags/rent_to_s3.py
+++ dags/rent_to_s3.py
@@ -9,7 +9,11 @@ import io
 import pandas as pd
 from datetime import date 
 
+logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.DEBUG)
 
+logger = logging.getLogger(__name__)
 
 
 def day_of_week():

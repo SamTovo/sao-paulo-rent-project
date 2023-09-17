663406b426570f092f840aed0498cb6818c92753
Samuel Tovo <samuel.gatti.tovo@gmail.com>
2023-09-17 10:34:09 -0300

    testing emr

--- dags/teste.py
+++ dags/teste.py
@@ -1,19 +1,66 @@
-from airflow import DAG
-from airflow.hooks.S3_hook import S3Hook
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
-from pyspark.sql import SparkSession, functions
 
-
-# Create an Airflow DAG
-dag = DAG(
-    "upload_parquet_to_s3_dag",
-    default_args={
-        "owner": "your-name",
-        "start_date": datetime(2023, 1, 1),
-        # Add any other default_args as needed
-    },
-    schedule_interval=None,  # You can set a schedule if needed
-    catchup=False,
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.emr import (
+    EmrServerlessCreateApplicationOperator,
+    EmrServerlessStartJobOperator,
+    EmrServerlessDeleteApplicationOperator,
 )
 
+# Replace these with your correct values
+JOB_ROLE_ARN = "arn:aws:iam::account-id:role/emr_serverless_default_role"
+S3_LOGS_BUCKET = "project-emr-studio-rent"
+DEFAULT_MONITORING_CONFIG = {
+    "monitoringConfiguration": {
+        "s3MonitoringConfiguration": {"logUri": f"s3://{S3_LOGS_BUCKET}/logs/"}
+    },
+}
+
+with DAG(
+    dag_id="example_endtoend_emr_serverless_job",
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    tags=["example"],
+    catchup=False,
+) as dag:
+    create_app = EmrServerlessCreateApplicationOperator(
+        task_id="create_spark_app",
+        job_type="SPARK",
+        release_label="emr-6.7.0",
+        config={"name": "airflow-test"},
+    )
+
+    application_id = create_app.output
+
+    job1 = EmrServerlessStartJobOperator(
+        task_id="start_job_1",
+        application_id=application_id,
+        execution_role_arn=JOB_ROLE_ARN,
+        job_driver={
+            "sparkSubmit": {
+                "entryPoint": "local:///usr/lib/spark/examples/src/main/python/pi_fail.py",
+            }
+        },
+        configuration_overrides=DEFAULT_MONITORING_CONFIG,
+    )
+
+    job2 = EmrServerlessStartJobOperator(
+        task_id="start_job_2",
+        application_id=application_id,
+        execution_role_arn=JOB_ROLE_ARN,
+        job_driver={
+            "sparkSubmit": {
+                "entryPoint": "local:///usr/lib/spark/examples/src/main/python/pi.py",
+                "entryPointArguments": ["1000"]
+            }
+        },
+        configuration_overrides=DEFAULT_MONITORING_CONFIG,
+    )
+
+    delete_app = EmrServerlessDeleteApplicationOperator(
+        task_id="delete_app",
+        application_id=application_id,
+        trigger_rule="all_done",
+    )
+
+    (create_app >> [job1, job2] >> delete_app)

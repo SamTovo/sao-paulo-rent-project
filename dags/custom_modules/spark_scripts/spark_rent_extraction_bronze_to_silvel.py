042d2e8becf209001164984ef2c13702e734e3e6
Samuel Tovo <samuel.gatti.tovo@gmail.com>
2023-09-17 21:52:22 -0300

    fix spark

--- dags/custom_modules/spark_scripts/spark_rent_extraction_bronze_to_silvel.py
+++ dags/custom_modules/spark_scripts/spark_rent_extraction_bronze_to_silvel.py
@@ -7,19 +7,19 @@ logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)
     datefmt='%Y-%m-%d:%H:%M:%S',
     level=logging.DEBUG)
 
-logger = logging.getLogger("airflow.task")
+logger = logging.getLogger()
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 gcs_bucket = "gs://rent-extraction"
 parquet_file_path = "bronze/scraped_rent_sp_*"
 logger.info("Writing Silver Table")
 df = spark.read.parquet(f"{gcs_bucket}/{parquet_file_path}")\
-    .withcolumn("price",regexp_replace("price","R$",""),trim("price"),col("price").cast("Integer"))\
-    .withcolumn("total_price",regexp_replace("total_price","R$",""),trim("total_price"),col("price").cast("Integer"))\
-    .withcolumn("address",col("address").cast("String"))\
-    .withcolumn("address",col("address").cast("String"))\
-    .withcolumn("floor_size",regexp_replace("floor_size","M2",""),trim("floor_size"),col("floor_size").cast("Integer"))\
-    .withcolumn("number_of_rooms",trim("number_of_rooms"),col("number_of_rooms").cast("Integer"))\
-    .withcolumn("number_of_bathrooms",trim("number_of_bathrooms"),col("number_of_bathrooms").cast("Integer"))
+    .withColumn("price",regexp_replace("price","R$",""),trim("price"),col("price").cast("Integer"))\
+    .withColumn("total_price",regexp_replace("total_price","R$",""),trim("total_price"),col("price").cast("Integer"))\
+    .withColumn("address",col("address").cast("String"))\
+    .withColumn("address",col("address").cast("String"))\
+    .withColumn("floor_size",regexp_replace("floor_size","M2",""),trim("floor_size"),col("floor_size").cast("Integer"))\
+    .withColumn("number_of_rooms",trim("number_of_rooms"),col("number_of_rooms").cast("Integer"))\
+    .withColumn("number_of_bathrooms",trim("number_of_bathrooms"),col("number_of_bathrooms").cast("Integer"))
     
 df.write.mode("overwrite").parquet(f"{gcs_bucket}/silver/silver_rent_extraction")
     
